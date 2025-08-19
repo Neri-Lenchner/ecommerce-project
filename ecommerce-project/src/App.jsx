e5256938564238e6a1595541cfda9ee5efe4ef46
neri <n.l.y.a.lench@gmail.com>
2025-08-19 10:45:35 +0300

    test checkout page 00:38:56

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import './App.css'
 
@@ -6,7 +6,11 @@ function App() {
 
 
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="checkout" element={<div>Test checkout page</div>}></Route>
+    </Routes>
+
   )
 }
 
